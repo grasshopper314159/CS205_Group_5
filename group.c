162d6cbd5a462836d220abd921cfd91361235818
Unknown <bosscodycollege@yahoo.com>
2019-02-05 14:47:42 -0800

    Major

--- group.c
+++ group.c
@@ -12,7 +12,7 @@ void jason() {
 }
 
 void cody() {
-  printf("\nName: Cody Hill-Boss\nYear in school: Junior\n");
+  printf("\nName: Cody Hill-Boss\nYear in school: Junior\nMajor: Computer Science\n");
 }
 
 void hannah() {

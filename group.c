2167ed5d6fbfaafa6d257af40e099471a5a33f2c
hollenbackhd <37252508+hollenbackhd@users.noreply.github.com>
2019-02-05 14:54:46 -0700

    Add files via upload
    Adding Name, year in school, major, where I grew up

---
+++ group.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+
+
+printf("Name: Hannah Hollenback\mYear in School: Junior\nMajor: Computer Science\nWhere I gre up: Superior, MT\nSomething interesting: Nothing interesting\n");

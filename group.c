502e7a61579995e6d850353953c3144011e55b6c
hollenbackhd <37252508+hollenbackhd@users.noreply.github.com>
2019-02-05 15:01:14 -0700

    Update group.c
    Added hobbies

--- group.c
+++ group.c
@@ -5,4 +5,5 @@ printf("Name: Hannah Hollenback\n"
        "Year in School: Junior\n"
        "Major: Computer Science\n"
        "Where I grew up: Superior, MT\n"
+       "Hobbies: Painting, Hiking, Camping\n"
        "Something interesting: Nothing interesting\n");

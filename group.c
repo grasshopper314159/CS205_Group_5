2b2fb69f28bb60663a4967a4fad557c9035340ad
Zatamataz <Sounararyke@gmail.com>
2019-02-06 11:50:46 -0700

    added hobbies and fixed a few things

--- group.c
+++ group.c
@@ -9,8 +9,9 @@ void josiah(void) {
 
 void jason(void) {
   printf("Jason Haroldson\n");
-  printf("3rd year in college");
-  printf("Computer Science Major");
+  printf("3rd year in college\n");
+  printf("Computer Science Major\n");
+  printf("Hobbies include reading, writing, animals\n");
 }
 
 void cody(void) {

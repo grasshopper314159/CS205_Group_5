0980fa9baaaf9032f5af23ba6888ee3fc236d36a
Josiah Hamer <thenitwillbe@hotmail.com>
2019-02-07 14:54:46 -0700

    Commit 4

--- group.c
+++ group.c
@@ -5,10 +5,11 @@
 
 void josiah() {
   
-printf("Josiah Hamer");
-printf("Junior");
-printf("Computer Science");
-printf("Hobbies are gaming, piano, making videos");
+printf("Josiah Hamer\n");
+printf("Junior\n");
+printf("Computer Science\n");
+printf("Hobbies are gaming, piano, making videos\n");
+printf("From Missoula\n");
 }
 
 void jason() {
